7271438715d948530ecc568a5ed15221410a8a38
Julien Cappiello <contact@kendozone.com>
2017-10-03 11:56:05 +0200

    ajout de commentaires

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+// Le commentaire 1
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -17,7 +18,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
-
+    // Le commentaire 2
     /**
      * The attributes that should be hidden for arrays.
      *
