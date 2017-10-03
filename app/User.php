8cf6eb8dd888edb0c9ca9d3c1ffdead842d9f68f
vincent <vincent.dupuis05@free.fr>
2017-10-03 17:51:05 +0200

    retour etat initial sur master

--- app/User.php
+++ app/User.php
@@ -3,8 +3,6 @@
 namespace App;
 
 // Le commentaire 1
-// Le commentaire vd1
-// Le commentaire vd2
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
