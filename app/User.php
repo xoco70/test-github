b2b0041f9deaebc5c65ae8616e655d591eee29e7
vincent <vincent.dupuis05@free.fr>
2017-10-03 17:16:31 +0200

    modifs 1&2

--- app/User.php
+++ app/User.php
@@ -3,6 +3,8 @@
 namespace App;
 
 // Le commentaire 1
+// Le commentaire vd1
+// Le commentaire vd2
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
