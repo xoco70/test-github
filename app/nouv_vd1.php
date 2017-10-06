f52369dfaf28542a7b48c8151d2ca4af0ee22965
vincent <vincent.dupuis05@free.fr>
2017-10-06 17:34:39 +0200

    remplace contenu fich nouv_vd1...

--- app/nouv_vd1.php
+++ app/nouv_vd1.php
@@ -1,11 +1,15 @@
-<?php
-
-namespace App;
-
-// Le commentaire 1
-// Le commentaire vd1
-// Le commentaire vd2
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-
-// fichier foireux pour test
+diff a/app/nouv_vd1.php b/app/nouv_vd1.php	(rejected hunks)
+@@ -2,10 +2,8 @@
+ 
+ namespace App;
+ 
+-// Le commentaire 1
+-// Le commentaire vd1
+-// Le commentaire vd2
++// petit changement apres 1.0.1
+ use Illuminate\Notifications\Notifiable;
+ use Illuminate\Foundation\Auth\User as Authenticatable;
+ 
+-// fichier foireux pour test
+\ No newline at end of file
++// fichier foireux pour test
